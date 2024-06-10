9d7663ca86970ab11c76534101924f74b71253b9
Sunga Thawethe <sungabt@gmail.com>
2024-06-10 12:57:45 +0200

    update product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,29 @@
 class Product < ApplicationRecord
+
+  
+    # Associations
+    has_many :order_items
+    has_many :stock_items
+  
+    # Attributes
+    attribute :name, :string
+    attribute :description, :string
+    attribute :category, :string
+    attribute :price, :decimal
+    attribute :sku, :string
+    attribute :stocklevel, :integer
+    attribute :reorderlevel, :integer
+  
+    # Validations
+    validates :name, presence: true
+    validates :description, presence: true, length: { maximum: 150 }
+    validates :category, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :stocklevel, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :reorderlevel, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  
+    # Methods
+  
+    # Callbacks
+
 end
