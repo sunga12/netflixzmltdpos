f9192e62a41e1f041448c101d593b92399c21c02
Sunga Thawethe <sungabt@gmail.com>
2024-06-10 13:04:43 +0200

    Update order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,21 @@
 class Order < ApplicationRecord
+  
+  
+  # Associations
   belongs_to :user
+  has_many :order_items
+  has_one :payment
+
+  # Attributes
+  attribute :date, :date
+  attribute :total, :decimal
+
+  # Validations
+
+  validates :total, numericality: { greater_than_or_equal_to: 0 }
+
+  # Methods
+
+  # Callbacks
+
 end
