2dccce15093a8ed84718b4d65053ad8df4ebd9ce
Sunga Thawethe <sungabt@gmail.com>
2024-06-10 13:19:26 +0200

    Update payment  model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,23 @@
 class Payment < ApplicationRecord
+  
+  # Associations 
+
   belongs_to :order
+
+  # Attributes
+
+  attribute :paymentmethod, :string
+  attribute :amount, :decimal
+
+  # Validations
+
+  validates :paymentmethod, presence: true
+  validates :amount, presence: true
+
+
+  # Methods
+
+  ## Sets the default to Order total
+
+  #
 end
