0088baeff6026b121a520c955bd66219858ddff0
Sunga Thawethe <sungabt@gmail.com>
2024-06-10 13:20:58 +0200

    Update stock-item model

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -1,3 +1,15 @@
 class StockItem < ApplicationRecord
   belongs_to :product
+
+
+# Attributes
+  attribute :changetype, :string
+  attribute :quantitychange, :integer
+  attribute :newstocklevel, :integer
+
+  # Validations 
+  validates :changetype, presence: true
+  validates :quantitychange, presence: true
+  validates :newstocklevel, presence: true
+
 end
